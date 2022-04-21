c33c1ef9568fe12aad91636a94579d8213db4fc5
yebeen <33bani@gmail.com>
2022-04-21 18:13:25 +0900

    Forder rename

--- 1_List/ArrayList/srcs/deletearraylist.c
+++
@@ -1,9 +0,0 @@
-#include "../include/arrayList.h"
-
-//모든 원소와 list를 지운다. 
-void deleteArrayList(ArrayList* pList) 
-{
-    clearArrayList(pList);
-    free(pList);
-    pList = NULL;
-}
--- 1_List/SinglyLinkedList/srcs/getlinkedlistlength.c
+++
@@ -1,9 +0,0 @@
-#include "../include/singlylinkedlist.h"
-
-/*linked list의 길이를 반환한다.*/
-int	getLinkedListLength(LinkedList *pList)
-{
-	if (!pList)
-		return (FALSE);
-	return (pList->currentElementCount);
-}
