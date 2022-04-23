0f541e4ee61f3382d22c54f4517cbcfe9f479300
yeblee <68188768+33bini@users.noreply.github.com>
2022-04-23 15:31:41 +0900

    init project

---
+++ 2_Stack/main.c
@@ -0,0 +1,8 @@
+#include "./includes/linkedstack.h"
+#include "./includes/arraystack.h"
+#include <stdio.h>
+
+int main(void) {
+    printf("hello stack!! :)\n");
+    return 0;
+}
---
+++ 2_Stack/src/arraystack.c
@@ -0,0 +1,1 @@
+#include "./includes/arraystack.h"
