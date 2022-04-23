723bf1c36c44230a9a0c18769397a3c7b6b2afb6
djeeee1272@gmail.com <djeeee1272@gmail.com>
2022-04-23 16:47:21 +0900

    Linked stack complete

--- 2_Stack/includes/linkedstack.h
+++ 2_Stack/includes/linkedstack.h
@@ -1,6 +1,8 @@
 #ifndef _LINKED_STACK_
 #define _LINKED_STACK_
 
+#include <stdlib.h>
+
 typedef struct StackNodeType
 {
 	char data;
@@ -9,7 +11,8 @@ typedef struct StackNodeType
 
 typedef struct LinkedStackType
 {
-	int currentElementCount;
+	size_t currentElementCount;
+	size_t maxElementCount;
 	StackNode* pTopElement;		
 } LinkedStack;
 
@@ -20,6 +23,9 @@ StackNode* peekLS(LinkedStack* pStack);
 void deleteLinkedStack(LinkedStack* pStack);
 int isLinkedStackFull(LinkedStack* pStack);
 int isLinkedStackEmpty(LinkedStack* pStack);
+
+void displayLinkedStack(LinkedStack *pStack);
+StackNode* createLinkedStackNode(char data);
 
 #endif // _LINKED_STACK_
 
--- 2_Stack/main.c
+++ 2_Stack/main.c
@@ -1,8 +1,24 @@
 #include "./includes/linkedstack.h"
-#include "./includes/arraystack.h"
+// #include "./includes/arraystack.h"
 #include <stdio.h>
+#include <string.h>
 
-int main(void) {
-    printf("hello stack!! :)\n");
+void testSpace_LinkedStack(char *argv) {
+    LinkedStack *ls = createLinkedStack(3);
+
+    for (size_t i = 0; i < strlen(argv); i++) {
+        StackNode *nptr = createLinkedStackNode(argv[i]);
+        pushLS(ls, *nptr);
+        free(nptr);
+    }
+    displayLinkedStack(ls);
+    deleteLinkedStack(ls);
+    system("leaks test");
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) return 0;
+    printf("inserted input : [%s] :)\n", argv[1]);
+    testSpace_LinkedStack(argv[1]);
     return 0;
 }
