76500542455ece9afae4ee3889eef773d1c833c9
yebeen <33bani@gmail.com>
2022-05-02 11:22:23 +0900

    Init Deque

---
+++ 3_Queue/Linked/2_Deque/main.c
@@ -0,0 +1,5 @@
+#include "include/linkedqueue.h"
+
+int main(void) {
+    return 0;
+}
---
+++ 3_Queue/Linked/2_Deque/src/linkeddeque.c
@@ -0,0 +1,41 @@
+#include "../include/linkeddeque.h"
+
+LinkedDeque* createLinkedDeque() {
+
+}
+
+int insertFrontLD(LinkedDeque* pDeque, DequeNode element) {
+
+}
+
+int insertRearLD(LinkedDeque* pDeque, DequeNode element) {
+
+}
+
+DequeNode* deleteFrontLD(LinkedDeque* pDeque) {
+
+}
+
+DequeNode* deleteRearLD(LinkedDeque* pDeque) {
+
+}
+
+DequeNode* peekFrontLD(LinkedDeque* pDeque) {
+
+}
+
+DequeNode* peekRearLD(LinkedDeque* pDeque) {
+
+}
+
+void deleteLinkedDeque(LinkedDeque* pDeque) {
+
+}
+
+int isLinkedDequeFull(LinkedDeque* pDeque) {
+
+}
+
+int isLinkedDequeEmpty(LinkedDeque* pDeque) {
+
+}
